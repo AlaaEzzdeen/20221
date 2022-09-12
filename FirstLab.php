1d580291a25e1bff0047f8e8be0a92d8a6a4e79c
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2022-09-12 12:29:59 +0300

    Update FirstLab.php

--- FirstLab.php
+++ FirstLab.php
@@ -9,6 +9,8 @@
     echo "<br>";
     echo "<h1>"."This is a header"."</h1>";
     echo "<p>"."This a paragraph"."</p>";
+    define('TESTING', 'My name is alaa and I&apos;m testing the constants in PHP');
+    echo date("Y/m/d ------ l");
     ?>    
 </body>
 </html>
