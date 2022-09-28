4ec272934f39ea8f6c1f7c4ef289905553289469
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2022-09-28 09:18:55 +0300

    Doing Lab2

--- FirstLab.php
+++ FirstLab.php
@@ -10,7 +10,14 @@
     echo "<h1>"."This is a header"."</h1>";
     echo "<p>"."This a paragraph"."</p>";
     define('TESTING', 'My name is alaa and I&apos;m testing the constants in PHP');
-    echo date("Y/m/d ------ l");
+    echo date("Y / m / d ------ l");
+    const ALAA = "My name is alaa";
+    for ($i=0; $i < 10; $i++) {
+        echo ("<br>");
+        echo ("hello"." ".$i);
+    }
+
+    echo ("<br>".ALAA);
     ?>    
 </body>
 </html>
---
+++ SecondLab.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $n = 1;
+    $s = "1";
+    $n1 = $n + $s; 
+    //$n2 = $n1 + "10 little penguins";
+    $n3 = "hello";
+    $n4 = " world";
+    //$n5 = $n3+$n4;
+    $n6 = $n3.$n4;
+    echo "$n<br>";
+    //The output will be 1 as an integer
+    echo "$s<br>";
+    //The output will be 1 as a string
+    echo "$n1<br>";
+    //The output will be 2 it will just use 1 from $s as an integer next to the $n 1 because we used + instead of .
+    //echo "$n2<br>";
+    //An error occurs becuase there is a string of words added to integer.
+    echo "$n3<br>";
+    //it will print hello
+    echo "$n4<br>";
+    //it will print world
+    //echo "$n5<br>";
+    //It can't be added because the + is not the concatenation operand to add strings to eachother in php, it should be .
+    echo "$n6<br>";
+    ?>
+</body>
+</html>
