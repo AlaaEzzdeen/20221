d55e32650d0f9e0fa6e5fa5a1372c86a385cbe7e
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2022-11-16 09:33:29 +0300

    Update Lab_06.php
    Just adding and removing space

--- Labs/Lab_06.php
+++ Labs/Lab_06.php
@@ -10,9 +10,9 @@
             $ins = mysqli_query($con, "INSERT INTO 06_Lab (F_name, L_name, S_id, Major) 
             VALUES ('Belal', 'Elhalles', 4, 'NES')");
             echo "<p> Data has been inserted </p>";
-        } catch (mysqli_sql_exception) {
+            } catch (mysqli_sql_exception) {
             echo "<p> Data is already inserted </p>";
-        }  
+            }  
         } 
         else {
             echo "<p> wrong credentials</p>";
