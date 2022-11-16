88c850e58f22b695a249ef31ccaf61a5273b74e2
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2022-11-16 09:24:09 +0300

    Lab_06

---
+++ Labs/Lab_06.php
@@ -0,0 +1,21 @@
+<?php 
+    $Host_IP = "127.0.0.1";
+    $username="Alaa";
+    $password= "1234";
+    $database = "Lab_db";
+    $con = mysqli_connect($Host_IP, $username, $password, $database);
+
+        if($con) {
+            try {  
+            $ins = mysqli_query($con, "INSERT INTO 06_Lab (F_name, L_name, S_id, Major) 
+            VALUES ('Belal', 'Elhalles', 4, 'NES')");
+            echo "<p> Data has been inserted </p>";
+        } catch (mysqli_sql_exception) {
+            echo "<p> Data is already inserted </p>";
+        }  
+        } 
+        else {
+            echo "<p> wrong credentials</p>";
+        }
+    
+?>
